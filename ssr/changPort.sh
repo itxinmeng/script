42e90e738eade577cbe983ecee0a269c3391ceed
心梦 <30740774+itxinmeng@users.noreply.github.com>
2023-02-19 22:17:25 +0800

    Update changPort.sh

--- ssr/changPort.sh
+++ ssr/changPort.sh
@@ -4,6 +4,11 @@
 port=$1
 container_name=$1
 passwd=$2
+#协议
+protocol=auth_chain_c
+#混淆方式
+obfs=http_post
+
 
 ip=$(curl -s cip.cc |head -1|awk '{print $3}')
 
@@ -13,10 +18,10 @@ docker stop $(echo $1 -5 | bc)   &>/dev/null
 
 docker rm $(echo $1 -5 | bc)   &>/dev/null
 
-docker run -td --restart=always -p $port:$port -e PORT=$port -e PASSWORD=$passwd --name $container_name itxinmeng/ssr-server &>/dev/null
+docker run -td --restart=always -p $port:$port -e PORT=$port -e PASSWORD=$passwd -e PROTOCOL=$protocol -e OBFS=$obfs --name $container_name itxinmeng/ssr-server &>/dev/null
 
 if [ $? -eq 0 ];then
-    ssr="$ip:$port:auth_aes128_sha1:aes-256-cfb:plain:$passwd_base64/?obfsparam=&remarks=eGlubWVuZw&group=eGlubWVuZy1zc3I"
+    ssr="$ip:$port:$protocol:aes-256-cfb:$obfs:$passwd_base64/?obfsparam=&remarks=eGlubWVuZw&group=eGlubWVuZy1zc3I"
     base64_ssr=$(echo -n $ssr|base64)
     link=$(echo -n $base64_ssr| sed s/[[:space:]]//g|sed s#/#_#g|sed s/+/-/g|sed 's/=//g')
     echo "ssr://$link"
